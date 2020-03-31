5eb0c7b68d226c85bec8d29e466bec3d687e3af7
cegijon <53779413+cegijon@users.noreply.github.com>
2020-03-31 22:40:58 +1100

    web adopcion gatitos con React

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Card = props => {
+    return (
+        <div className="card">
+        <div className="card_img">
+     <img src={props.info.img}/>
+        </div>
+        <div className="card_info">
+          <h3>{props.info.name}</h3>
+          <p>
+            {props.info.shortDesc} 
+          </p>
+          <button>Ver mas</button>
+        </div>
+      </div>
+    )
+}
+
+
+export default Card;
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import icono from '../img/burgerbutton.png';
+
+const NavBar = () => {
+return (
+    <nav>
+    <div>#AdoptAdas</div>
+    <img src={icono}></img>
+  </nav>
+)
+}
+
+
+export default NavBar;
